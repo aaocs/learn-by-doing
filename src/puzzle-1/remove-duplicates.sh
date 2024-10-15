087813a4c34eb3e3b0d2676f98cca7c78a3038ba
aaocs <132440545+aaocs@users.noreply.github.com>
2024-10-15 16:24:40 +0100

    chore: add answers as comments in remove-duplicates.sh for puzzle 1

--- src/puzzle-1/remove-duplicates.sh
+++ src/puzzle-1/remove-duplicates.sh
@@ -8,3 +8,11 @@
 # -s skips the first N characters during comparison
 # -D prints all duplicate lines
 sort -t, -d -k2 "$1" | uniq -s 20 -D
+
+#./remove-duplicates.sh res/puzzle.csv
+#6440 0FY9 TEVX VZ2B,b5ed3762-9c52-4a3c-a3b2-ca5aeed9056e
+#6449 CV4F AB7B 2VEK,b5ed3762-9c52-4a3c-a3b2-ca5aeed9056e
+
+#./remove-duplicates.sh res/puzzle_bonus.csv
+#64G3 QCZF HBAM NEWQ,27a7edab-1c35-4ddc-b28e-e8cc2c552480
+#64G7 XH56 3HPM M8CN,27a7edab-1c35-4ddc-b28e-e8cc2c552480
