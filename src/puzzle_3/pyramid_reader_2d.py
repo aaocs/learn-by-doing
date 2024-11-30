449a79e38c6516e3efe02fa883480b9558e24b80
aaocs <132440545+aaocs@users.noreply.github.com>
2024-11-30 17:48:31 +0000

    wip: add failing tests for pyramid reader

--- src/puzzle_3/pyramid_reader_2d.py
+++ src/puzzle_3/pyramid_reader_2d.py
@@ -5,5 +5,45 @@ class PyramidReader:
         self.file = file
         self.graph = self._construct_graph()
 
+    def _triangle_number(self, n):
+        return n * (n + 1) // 2
+
     def _construct_graph(self):
-        return Graph(1) #TODO.
+        all_rows = []
+
+        input = open(self.file, "r")
+
+        for line in input:
+            list = line.rstrip().split(" ")
+            all_rows.append(list)
+
+        input.close()
+
+        n = len(all_rows)
+        size = self._triangle_number(n) + 1
+        graph = Graph(size)
+
+        # Add vertex data.
+        vertex_counter = 0 # Vertices are 0-indexed.
+        for i, row in enumerate(all_rows, start=1):
+            for j, node in enumerate(row, start=1):
+                graph.add_vertex_data(vertex_counter, '[{0}][{1}]'.format(i, j))
+                vertex_counter += 1
+
+        # Add edges.
+        for i, row in enumerate(all_rows, start=1):
+            prev_t_num = self._triangle_number(i - 1)
+            curr_t_num = self._triangle_number(i)
+
+            for j, node in enumerate(row, start=1):
+                # Vertices are 0-indexed.
+                if i == len(all_rows):
+                    # Connect all nodes on the bottom row to the top node.
+                    graph.add_edge(prev_t_num + j - 1, 0, int(all_rows[1][1], 16))
+                else:
+                    graph.add_edge(prev_t_num + j - 1, curr_t_num + j - 1, int(all_rows[i][j - 1], 16))
+                    graph.add_edge(prev_t_num + j - 1, curr_t_num + j, int(all_rows[i][j], 16))
+
+
+        #print(all_rows)
+        return graph
--- test/puzzle_3/pyramid_reader_2d_test.py
+++ test/puzzle_3/pyramid_reader_2d_test.py
@@ -2,14 +2,23 @@ import unittest
 from ...src.puzzle_3.pyramid_reader_2d import PyramidReader
 
 class TestStringMethods(unittest.TestCase):
+    FILE_PATH_PREFIX = '/Users/ashley.arain/git-proj/learn-by-doing/src/puzzle_3/res/'
 
     def test_edge_count_example_1(self):
-        pyramid_reader = PyramidReader('example1.txt')
+        pyramid_reader = PyramidReader(self.FILE_PATH_PREFIX + 'example1.txt')
         self.assertEqual(pyramid_reader.graph.edge_count, 11)
 
     def test_edge_count_part_1(self):
-        pyramid_reader = PyramidReader('part.txt')
+        pyramid_reader = PyramidReader(self.FILE_PATH_PREFIX + 'part1.txt')
         self.assertEqual(pyramid_reader.graph.edge_count, 79)
+
+    def test_maximum_sum_example_1(self):
+        pyramid_reader = PyramidReader(self.FILE_PATH_PREFIX + 'example1.txt')
+
+        path, distances, max_distance = pyramid_reader.graph.dijkstra('[1][1]', '[4][4]')
+        print(f"Path: {path}, Distances: {distances}, Maximum Distance: {max_distance}")
+
+        self.assertEqual(max_distance, 23)
 
 if __name__ == '__main__':
     unittest.main()
