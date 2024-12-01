bd86ff89848df1656bb27e8875032dd27e1ed163
aaocs <132440545+aaocs@users.noreply.github.com>
2024-12-01 18:33:39 +0000

    feat: add _construct_graph functionality for 3d pyramid reader

--- src/puzzle_3/pyramid_reader.py
+++ src/puzzle_3/pyramid_reader.py
@@ -1,5 +1,14 @@
 from .graph import Graph
 
+def _add_vertex_data(graph, list_of_lists):
+    vertex_counter = 0 # Vertices are 0-indexed.
+    for i, row in enumerate(list_of_lists, start=1):
+        for j, node in enumerate(row, start=1):
+            graph.add_vertex_data(vertex_counter, '[{0}][{1}]'.format(i, j))
+            vertex_counter += 1
+    graph.add_vertex_data(vertex_counter, '[END]')
+    return vertex_counter
+
 class PyramidReader2D:
     def __init__(self, file):
         self.file = file
@@ -24,12 +33,7 @@ class PyramidReader2D:
         graph = Graph(size)
 
         # Add vertex data.
-        vertex_counter = 0 # Vertices are 0-indexed.
-        for i, row in enumerate(all_rows, start=1):
-            for j, node in enumerate(row, start=1):
-                graph.add_vertex_data(vertex_counter, '[{0}][{1}]'.format(i, j))
-                vertex_counter += 1
-        graph.add_vertex_data(vertex_counter, '[END]')
+        vertex_counter = _add_vertex_data(graph, all_rows)
 
         # Add edges.
         for i, row in enumerate(all_rows, start=1):
@@ -42,8 +46,8 @@ class PyramidReader2D:
                     # Connect all nodes on the bottom row to the top node.
                     graph.add_edge(prev_t_num + j - 1, vertex_counter, int(all_rows[0][0], 16))
                 else:
-                    graph.add_edge(prev_t_num + j - 1, curr_t_num + j - 1, int(all_rows[i][j - 1], 16))
-                    graph.add_edge(prev_t_num + j - 1, curr_t_num + j, int(all_rows[i][j], 16))
+                    for k in [j - 1, j]:
+                        graph.add_edge(prev_t_num + j - 1, curr_t_num + k, int(all_rows[i][k], 16))
 
         return graph
 
@@ -56,4 +60,55 @@ class PyramidReader3D:
         return n * (n + 1) * ((2 * n) + 1) // 6
 
     def _construct_graph(self):
-        return Graph(0) # TODO.
+        all_planes = []
+
+        input = open(self.file, "r")
+
+        plane_counter = 1
+        prev_plane_size = 0
+        plane_size = 1
+        current_plane = []
+        for line in input:
+            list = line.rstrip().split(" ")
+            current_plane += list
+
+            if len(current_plane) == plane_size - prev_plane_size:
+                all_planes.append(current_plane)
+                current_plane = []
+                plane_counter += 1
+                prev_plane_size = plane_size
+                plane_size = self._pyramid_number(plane_counter)
+
+        input.close()
+
+        n = len(all_planes)
+        size = self._pyramid_number(n) + 1
+        graph = Graph(size)
+
+        # Add vertex data.
+        vertex_counter = _add_vertex_data(graph, all_planes)
+
+        # Add edges.
+        for i, plane in enumerate(all_planes, start=1):
+            prev_p_num = self._pyramid_number(i - 1)
+            curr_p_num = self._pyramid_number(i)
+            column_counter = -1 # This will equal 0 after the first if-check.
+
+            for j, node in enumerate(plane, start=1):
+                # Vertices are 0-indexed.
+                if i == len(all_planes):
+                    # Connect all nodes on the bottom plane to the top node.
+                    graph.add_edge(prev_p_num + j - 1, vertex_counter, int(all_planes[0][0], 16))
+                else:
+                    if (j - 1) % i == 0:
+                        column_counter += 1
+
+                    for k in [
+                        column_counter + j - 1,
+                        column_counter + j,
+                        column_counter + j + i,
+                        column_counter + j + i + 1
+                    ]:
+                        graph.add_edge(prev_p_num + j - 1, curr_p_num + k, int(all_planes[i][k], 16))
+
+        return graph
